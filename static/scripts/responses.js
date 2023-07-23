de8b28353e92fcea894e2c78f5d361a9798b381a
Sandeep Kumar J <SandeepKumar.J@sabre.com>
2023-07-23 10:43:10 +0530

    re-enable send

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -19,7 +19,7 @@ function getBotResponse(input) {
 }
 
 
-var baseUrl = 'https://knowledge-bot-2aptfnllba-uc.a.run.app/'
+var baseUrl = 'https://knowledge-bot-2aptfnllba-uc.a.run.app'
 
 
 function callServer(input){
@@ -33,7 +33,7 @@ function callServer(input){
 
     console.log("sent info \n" + localStorage.getItem("token"));
     console.log("sent request to:\n" + url);
-    // request.send();
+    request.send();
     request.onload = (e) => {
         console.log("received response " + request.getAllResponseHeaders())
         console.log("received response " + request.responseText)
